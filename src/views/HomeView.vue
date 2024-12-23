7eb672058bb62580d0e6fdd7ed0a6b018b40fd34
Noman Mia <nomanislamnishad@gmail.com>
2024-12-23 20:29:55 +0600

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+  <div class="">
+
+    <div>
+      <nav class="uppercase h-20 bg-blue-500 text-white font-bold text-3xl flex items-center justify-center">
+            Todo App
+        </nav>
+    </div>
+
+    <div class="flex items-center justify-center">
+        <div class="w-8/12 border border-green-400 ">
+             <input type="text" placeholder="Your Todo">
+        </div>
+    </div>
+
+    
+  </div>
+  </main>
+</template>
